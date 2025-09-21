f132358038f89b9ad0fd35bdb1e3f438e03251e8
howon720 <rlaghdnjs17@naver.com>
2025-09-21 17:38:24 +0900

    read

--- checkpoints/wav2lip_gan/Wav2Lip-SD-GAN/code/__torch__/torch/nn/modules/activation/___torch_mangle_115.py
+++
@@ -1,8 +0,0 @@
-class ReLU(Module):
-  __parameters__ = []
-  __buffers__ = []
-  training : bool
-  _is_full_backward_hook : Optional[bool]
-  def forward(self: __torch__.torch.nn.modules.activation.___torch_mangle_115.ReLU,
-    input: Tensor) -> Tensor:
-    return torch.relu(input)
